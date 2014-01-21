198719fd4b7c0e64aebb77aff4fdacfa0faab246
rudametw <rudametkin@gmail.com>
2014-01-21 14:19:10 +0100

    Initial commit

---
+++ tpmaven/src/main/java/fr/triskell/tpm1miage/tpmaven/App.java
@@ -0,0 +1,13 @@
+package fr.triskell.tpm1miage.tpmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
